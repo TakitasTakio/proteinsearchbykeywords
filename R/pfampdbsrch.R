58a589161e890e6cdf6b017174db115c03d4619d
Takio W <takiyono@outlook.com>
2019-01-01 20:17:33 -0500

    function condition added

--- R/pfampdbsrch.R
+++ R/pfampdbsrch.R
@@ -36,9 +36,29 @@ obtain_from_keywords <- function(keywords="immunity"){
 
 
 obtain_pdb_from_keyword = function(searchItem="immunity") {
-  defense.data=obtain_from_keywords(keywords = searchItem)
-  pfam.to.pdb_no= data.frame(PFAMPDB)
-  conv.to.pdb=function(x){
+  if(is.null(searchItem) ){
+    warning("Please enter keywords as character string, e.g. 'immunity'")
+  }
+  if(is.numeric(searchItem)){
+    warning("Keywords cannot be numeric, please enter keywords as character string, e.g. 'immunity'")
+  }
+  if(str_detect(searchItem, "\\s")){
+    warning("If your keywors include whitespace in it, please use '+' instead of whitespace , e.g. the format should be like 'heart+disease'")
+  }
+
+  else{
+    url.start="https://pfam.xfam.org/search/keyword?query="
+    url.end="&submit=Submit"
+    url.1=paste(url.start, searchItem, url.end, sep = "")
+    md=url.1%>% read_html(encoding = "UTF-8") %>% html_nodes("table.resultTable")
+    md.pfam=md%>%html_nodes("tbody>tr>td:nth-child(2)") %>%html_text
+    md.id=md%>%html_nodes("tbody>tr>td:nth-child(3)") %>%html_text
+    md.dsp=md%>%html_nodes("tbody>tr>td:nth-child(4)") %>%html_text
+    defense.data=data.frame(md.pfam, md.id, md.dsp)
+    colnames(defense.data)=c('pfam-ACCN', 'ID', 'Description')
+
+    pfam.to.pdb_no= data.frame(PFAMPDB)
+    conv.to.pdb=function(x){
     return(
       pfam.to.pdb_no$pdb[pfam.to.pdb_no$ac==x]
     )
@@ -47,13 +67,40 @@ obtain_pdb_from_keyword = function(searchItem="immunity") {
   defense.pdb=lapply(defense.data$pdb_id, conv.to.pdb)
   print(defense.pdb)
   ########"The pdb list order indexes correspond to the order index number of pfams, please use obtain_full_pfampdb() to get the full table"
-
+}
 }
 
 obtain_full_pfampdb=function(item="immunity"){
-  defense.data=obtain_from_keywords(keywords = item)
-  defense.pdb=obtain_pdb_from_keyword(searchItem =  item)
-  new.defense.pdb=defense.pdb
+  if(is.null(item) ){
+    warning("Please enter keywords as character string, e.g. 'immunity'")
+  }
+  if(is.numeric(item)){
+    warning("Keywords cannot be numeric, please enter keywords as character string, e.g. 'immunity'")
+  }
+  if(str_detect(item, "\\s")){
+    warning("If your keywors include whitespace in it, please use '+' instead of whitespace , e.g. the format should be like 'heart+disease'")
+  }
+  else{
+    url.start="https://pfam.xfam.org/search/keyword?query="
+    url.end="&submit=Submit"
+    url.1=paste(url.start, item, url.end, sep = "")
+    md=url.1%>% read_html(encoding = "UTF-8") %>% html_nodes("table.resultTable")
+    md.pfam=md%>%html_nodes("tbody>tr>td:nth-child(2)") %>%html_text
+    md.id=md%>%html_nodes("tbody>tr>td:nth-child(3)") %>%html_text
+    md.dsp=md%>%html_nodes("tbody>tr>td:nth-child(4)") %>%html_text
+    defense.data=data.frame(md.pfam, md.id, md.dsp)
+    colnames(defense.data)=c('pfam-ACCN', 'ID', 'Description')
+
+    pfam.to.pdb_no= data.frame(PFAMPDB)
+    conv.to.pdb=function(x){
+      return(
+        pfam.to.pdb_no$pdb[pfam.to.pdb_no$ac==x]
+      )
+    }
+    defense.data$pdb_id=defense.data$`pfam-ACCN`
+    defense.pdb=lapply(defense.data$pdb_id, conv.to.pdb)
+
+      new.defense.pdb=defense.pdb
   for(i in 1:length(defense.pdb)){
     new.defense.pdb[i]= paste(defense.pdb[i], collapse = '')
   }
@@ -66,6 +113,7 @@ obtain_full_pfampdb=function(item="immunity"){
   defense.data$pdb_id=new.defense.pdb
   print(defense.data)
 
+  }
 }
 
 
